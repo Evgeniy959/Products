62586ddb70d39cdf2d30dd629fb9925387bf6549
Starinin Andrey <starininandrey@gmail.com>
2022-01-15 12:43:42 +0300

    Update sql

--- SQL/create_tables.sql
+++ SQL/create_tables.sql
@@ -11,3 +11,28 @@ CREATE TABLE tab_products (
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
+
+-- Тестовые данные
+
+-- Тестовые типы продуктов
+INSERT INTO host1323541_sbd17.tab_types (name)
+VALUES ('prod_1');
+
+INSERT INTO host1323541_sbd17.tab_types (name)
+VALUES ('prod_2');
+
+INSERT INTO host1323541_sbd17.tab_types (name)
+VALUES ('prod_3');
+
+-- Тестовые продукты
+INSERT INTO host1323541_sbd17.tab_products (name, id_type)
+VALUES ('product_1', 1);
+
+INSERT INTO host1323541_sbd17.tab_products (name, id_type)
+VALUES ('product_2', 3);
+
+INSERT INTO host1323541_sbd17.tab_products (name, id_type)
+VALUES ('product_3', 2);
+
+INSERT INTO host1323541_sbd17.tab_products (name, id_type)
+VALUES ('product_4', 3);
