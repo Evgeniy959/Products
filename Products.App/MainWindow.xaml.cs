42ffa9f4a4c2625dbdad1a95acda6c62026b301b
Starinin Andrey <starininandrey@gmail.com>
2022-01-15 13:40:31 +0300

    Add binding

--- Products.App/MainWindow.xaml.cs
+++ Products.App/MainWindow.xaml.cs
@@ -1,12 +1,26 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using Products.Model;
 
 namespace Products.App
 {
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Product> _products;
+        private ObservableCollection<ProductType> _types;
+        private ProductsDB _db;
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            _db = new ProductsDB();
+            _products = new ObservableCollection<Product>(_db.TabProducts);
+            _types = new ObservableCollection<ProductType>(_db.TabTypes);
+
+            ProductsList.ItemsSource = _products;
+            ProductType.ItemsSource = _types;
         }
     }
 }
